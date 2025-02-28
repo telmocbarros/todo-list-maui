01ce53cfbd65f3a52d322c6197068c2ce6823834
Telmo Barros <Telmo Barros@DESKTOP-M1CLBNT>
2025-02-28 14:35:15 -0800

    Add project files.

---
+++ todo-list-maui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace todo_list_maui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ todo-list-maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace todo_list_maui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
