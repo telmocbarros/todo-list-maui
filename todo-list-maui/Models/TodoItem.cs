637bce61036cb5720bdc81f8118930ca880158ca
telmo barros <somebodyceo@gmail.com>
2025-03-01 10:06:53 +0100

    create todo model

---
+++ todo-list-maui/Models/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace todo_list_maui.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
