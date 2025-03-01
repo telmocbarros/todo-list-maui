af6ad7d7ad669e598ccf0acb8e28cfbbd47e2952
telmo barros <somebodyceo@gmail.com>
2025-03-01 11:57:24 +0100

    inject services into app

--- todo-list-maui/App.xaml.cs
+++ todo-list-maui/App.xaml.cs
@@ -1,10 +1,19 @@
-﻿namespace todo_list_maui
+﻿using todo_list_maui.Services;
+using todo_list_maui.View_Model;
+using todo_list_maui.Views;
+
+namespace todo_list_maui
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+            var builder = MauiApp.CreateBuilder();
+            builder.Services.AddScoped<ITodoService, TodoService>();
+
+            var app = builder.Build();
+            app.Services.GetService<TodoService>();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
